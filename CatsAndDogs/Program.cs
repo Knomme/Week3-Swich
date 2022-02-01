97857749a788e4f77e6c9cc7c64edace982ef6fe
Keit Nõmme <knomme@tktk.ee>
2022-02-01 17:03:30 +0200

    add new projects

--- CatsAndDogs/Program.cs
+++ CatsAndDogs/Program.cs
@@ -4,26 +4,25 @@ namespace CatsAndDogs
 {
 	class Program
 	{
-		static void Main(string[] args)
+	    static void Main(string[] args)
 		{
             
-            //a
-            
-            Console.WriteLine("Are you more like a cat or a dog?");
-            string userChoice = Convert.ToString(Console.ReadLine().ToLower());
+                //a
+                Console.WriteLine("Are you more like a cat or a dog?");
+                string userChoice = Convert.ToString(Console.ReadLine().ToLower());
 
-            if (userChoice == "cat")
-            {
+                if (userChoice == "cat")
+                {
                 Console.WriteLine("You are a home-lover.");
-            }
-            else if (userChoice == "DOG")
-            {
+                }
+                else if (userChoice == "DOG")
+                {
                 Console.WriteLine("You are a partygoer.");
-            }
-            else
-            {
+                }
+                else
+                {
                 Console.WriteLine($"You are a {userChoice} friend.");
-            }
+                }
         }
 	}
 }
